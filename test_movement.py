7c9f9a54d5471cfaf15cbd219078da4f33abb3b2
steph <stephlojano@gmail.com>
2021-04-13 23:48:34 -0400

    modified code

--- test_movement.py
+++ test_movement.py
@@ -1,30 +1,4 @@
-import movement
-
-go_ahead(100)
-time.sleep(1)
-stop_car()
-
-go_back(100)
-time.sleep(1)
-stop_car()
-
-turn_left(100)
-time.sleep(1)
-stop_car()
-
-turn_right(100)
-time.sleep(1)
-stop_car()
-
-shift_right(100)
-time.sleep(1)
-stop_car()
-
-shift_left(100)
-time.sleep(1)
-stop_car()
-
-change_duty_cycle(50)
+from movement import *
 
 go_ahead(100)
 time.sleep(1)
